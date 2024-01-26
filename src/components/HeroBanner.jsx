96f05199ca46e18dab503123d86bb5d725e6b6e5
Sara <saraatiyabi18@gmail.com>
2024-01-26 04:34:20 -0800

    modifying typography in small devices

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -11,7 +11,7 @@ const HeroBanner = () => (
         <Typography fontWeight={600} fontSize="40px" mb={3}>Sweat, Smile<br /> and Repeat</Typography>
         <Typography fontWeight={600} my={3}>Check Out The Most Effective Exercises</Typography>
         <Button variant='contained' mb={1} href='#exercises' sx={{ backgroundColor: "#ff2625", padding: "10px" }}>EXPLORE EXERCISES</Button>
-        <Typography fontWeight={600} fontSize="160px" color="#ff2625" sx={{ opacity: "0.1", display: { lg: "block", xs: "hidden" } }}>Exercise</Typography>
+        <Typography fontWeight={600} fontSize="160px" color="#ff2625" sx={{ opacity: "0.1", display: { lg: "block", xs: "none" } }}>Exercise</Typography>
         <img src={banner} alt='banner' className='hero-banner-img' />
     </Box>
 )
